d6124afb6186c0e7effc2fd9576f4194ce1f31be
xiongyx <1399852153@qq.com>
2022-03-18 23:02:14 +0800

    AST解析 二元表达式

--- compiler/src/main/java/parser/MyExpressionParser.java
+++ compiler/src/main/java/parser/MyExpressionParser.java
@@ -1,6 +1,6 @@
 package parser;
 
-import com.sun.org.apache.regexp.internal.RE;
+import com.sun.scenario.effect.Merge;
 import lexan.LexicalAnalyzer;
 import lexan.enums.TokenTypeEnum;
 import lexan.model.Token;
@@ -27,7 +27,7 @@ public class MyExpressionParser {
     }
 
     public static void main(String[] args) {
-        String sourceCode = "1*2+3*4+5 T";
+        String sourceCode = "6+1*2+3*4+5 T";
         System.out.println(sourceCode);
         LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(sourceCode);
         List<Token> tokenList = lexicalAnalyzer.parseToken();
@@ -35,16 +35,6 @@ public class MyExpressionParser {
         MyExpressionParser myParser = new MyExpressionParser(new TokenReader(tokenList));
         ASTNode treeNode = myParser.parseExpression();
         treeNode.printTree();
-
-//        String sourceCode2 = "1+2*3 T";
-//        System.out.println(sourceCode2);
-//        LexicalAnalyzer lexicalAnalyzer2 = new LexicalAnalyzer(sourceCode2);
-//        List<Token> tokenList2 = lexicalAnalyzer2.parseToken();
-//
-//        MyExpressionParser myParser2 = new MyExpressionParser(new TokenReader(tokenList2));
-//        ASTNode treeNode2 = myParser2.parseExpression();
-//        treeNode2.printTree();
-
     }
 
     public ASTNode parseExpression(){
@@ -58,7 +48,6 @@ public class MyExpressionParser {
             Token token = tokenReader.peekToken();
             if (!binaryOpEnumMap.containsKey(token.getTokenTypeEnum())) {
                 // 发现不是二元操作符后退出
-                // 将整个栈中遗留的所有ASTNode按照顺序弹出，组装成一个AST返回
                 return mergeRemainingNode(opNumStack,binaryOpStack);
             }
 
@@ -72,28 +61,39 @@ public class MyExpressionParser {
                 ASTNode opNumNode2 = myParser.primary();
                 opNumStack.push(opNumNode2);
             }else{
-                // 当前二元符号优先级低于栈顶符号
-                // 弹出当前栈顶头两个操作数，将其与操作符号共同组成一个mergeNode
-                ASTNode opNumNode1 = opNumStack.pop();
-                ASTNode opNumNode2 = opNumStack.pop();
-                // 先弹出栈顶高优先级的操作符，用于生成mergeNode
-                BinaryOpEnum topOp = binaryOpStack.pop();
-                // 随后将当前低优先级的操作符压入操作符栈中
-                binaryOpStack.push(currentBinaryOpEnum);
+                do{
+                    // 当前二元符号优先级低于栈顶符号
+                    // 弹出当前栈顶头两个操作数，将其与操作符号共同组成一个mergeNode
+                    mergeNode(opNumStack, binaryOpStack, currentBinaryOpEnum);
+                    // merge后，若当前操作符的优先级依然低于栈顶操作符，则再进行规约，循环往复
+                }while(!comparePriorityLevel(binaryOpStack,currentBinaryOpEnum));
 
-                ASTNode opNode = new ASTNode(ASTNodeTypeEnum.CALCULATE_OP,topOp.getTokenTypeEnum().getMessage());
-                ASTNode mergeNode = new ASTNode(topOp.getAstNodeTypeEnum());
-                // 由于入栈是先进后出；因此后出栈的是左孩子节点；先出栈的是右孩子节点
-                mergeNode.appendChildren(opNumNode2)
-                    .appendChildren(opNode)
-                    .appendChildren(opNumNode1);
-
-                opNumStack.push(mergeNode);
+                // 随后将当前低优先级的操作符压入操作符栈中,下一个操作数压入操作数栈中
                 ASTNode nextOpNum = myParser.primary();
-                // 和下一个操作数
                 opNumStack.push(nextOpNum);
+                binaryOpStack.push(currentBinaryOpEnum);
             }
         }
+    }
+
+    /**
+     * 前二元符号优先级低于栈顶符号
+     * 弹出当前栈顶头两个操作数，将其与操作符号共同组成一个mergeNode
+     * */
+    private void mergeNode(Stack<ASTNode> opNumStack,Stack<BinaryOpEnum> binaryOpStack,BinaryOpEnum currentBinaryOpEnum){
+        ASTNode opNumNode1 = opNumStack.pop();
+        ASTNode opNumNode2 = opNumStack.pop();
+        // 先弹出栈顶高优先级的操作符，用于生成mergeNode
+        BinaryOpEnum topOp = binaryOpStack.pop();
+
+        ASTNode opNode = new ASTNode(ASTNodeTypeEnum.CALCULATE_OP,topOp.getTokenTypeEnum().getMessage());
+        ASTNode mergeNode = new ASTNode(topOp.getAstNodeTypeEnum());
+        // 由于入栈是先进后出；因此后出栈的是左孩子节点；先出栈的是右孩子节点
+        mergeNode.appendChildren(opNumNode2)
+                .appendChildren(opNode)
+                .appendChildren(opNumNode1);
+
+        opNumStack.push(mergeNode);
     }
 
     private BinaryOpEnum matchBinaryOp(){
@@ -128,20 +128,20 @@ public class MyExpressionParser {
      * 将整个栈中遗留的所有ASTNode按照顺序弹出，组装成一个AST返回
      * */
     private ASTNode mergeRemainingNode(Stack<ASTNode> opNumStack,Stack<BinaryOpEnum> binaryOpStack){
-        ASTNode mergeNode = opNumStack.pop();
-        // 不断迭代，直到操作符栈为空
-        // 注意：特殊情况，操作符栈一进来就是空的，则操作数栈必须只存在一个opNumNode，此时直接弹出返回即可
-        while(!binaryOpStack.isEmpty()){
-            ASTNode opNum1 = opNumStack.pop();
-            BinaryOpEnum topOp = binaryOpStack.pop();
-            ASTNode opNode = new ASTNode(ASTNodeTypeEnum.CALCULATE_OP,topOp.getTokenTypeEnum().getMessage());
-
-            ASTNode newMergeNode = new ASTNode(topOp.getAstNodeTypeEnum());
-            newMergeNode.appendChildren(opNum1)
-                    .appendChildren(opNode)
-                    .appendChildren(mergeNode);
-            mergeNode = newMergeNode;
+        if(binaryOpStack.isEmpty()){
+            // 特殊情况，操作符栈一进来就是空的，则操作数栈必须只存在一个opNumNode，此时直接弹出返回即可（可能存在吗？）
+            return opNumStack.pop();
         }
+
+        ASTNode opNum2 = opNumStack.pop();
+        ASTNode opNum1 = opNumStack.pop();
+        BinaryOpEnum topOp = binaryOpStack.pop();
+        ASTNode opNode = new ASTNode(ASTNodeTypeEnum.CALCULATE_OP,topOp.getTokenTypeEnum().getMessage());
+
+        ASTNode mergeNode = new ASTNode(topOp.getAstNodeTypeEnum());
+        mergeNode.appendChildren(opNum1)
+                    .appendChildren(opNode)
+                    .appendChildren(opNum2);
 
         if(!opNumStack.isEmpty()){
             throw new RuntimeException("opNumStack must empty！！！");
